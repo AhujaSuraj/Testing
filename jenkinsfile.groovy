55cfa5a7cd89537d6b023291af726a63697d24cd
Suraj Ahuja <Suraj.Ahuja@esailors.de>
2019-06-18 15:07:47 +0200

    jenkinsfile changedsdsddd

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,6 +6,16 @@ def builddate
 SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 String currentdate = simpleDateFormat.format(new Date());
 println currentdate;
+def item = Jenkins.instance.getItemByFullName("/Team/Suraj/mainjob-pipline/job1")
+if (item.getLastBuild()) {
+	ff=item.getLastSuccessfulBuild()
+	builddate=ff.getTime().format("dd-MM-yyyy")
+}
+else {
+	builddate=currentdate
+}
+println  'build date';
+println builddate;
 pipeline {
 	agent {
 		label 'DETerminal'
