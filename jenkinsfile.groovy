d37fefb21915841df0c9d8fa7ab9a1a580d63a37
Suraj Ahuja <Suraj.Ahuja@esailors.de>
2019-06-18 15:14:28 +0200

    WIP

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,18 +6,6 @@ def builddate
 SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 String currentdate = simpleDateFormat.format(new Date());
 println currentdate;
-def item = Jenkins.instance.getItemByFullName("/Team/Suraj/mainjob-pipline/job1");
-if (item.getLastBuild()) {
-	println 'getiing buil date';
-	ff=item.getLastSuccessfulBuild();
-	builddate=ff.getTime().format("dd-MM-yyyy");
-}
-else {
-	println 'system date';
-	builddate=currentdate;
-}
-println  'build date';
-println builddate;
 pipeline {
 	agent {
 		label 'DETerminal'
@@ -35,8 +23,17 @@ pipeline {
 		}
 		stage ('Lastsuccess') {
 			steps{
-				
-				echo 'ecdfdfd'
+				script{
+					def item = Jenkins.instance.getItemByFullName("/Team/Suraj/mainjob-pipline/job1")
+					if (item.getLastBuild()) {
+							def ff=item.getLastSuccessfulBuild()
+						    builddate=ff.getTime().format("dd-MM-yyyy")
+					}
+					else {
+						builddate=currentdate
+					}
+				}
+				echo $[builddate}
 			}
 		}
 		stage('DWHJOBS'){
